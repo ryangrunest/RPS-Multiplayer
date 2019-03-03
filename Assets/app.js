065eff525efd52ca332b40c42dc85ddc4df9105d
Ryan Grunest <ryangrunest45@gmail.com>
2019-03-03 11:08:10 -0800

    alpha

--- Assets/app.js
+++ Assets/app.js
@@ -12,6 +12,7 @@ var config = {
   var player;
   var p1 = firebase.database().ref('player1');
   var p2 = firebase.database().ref('player2');
+  var winner = firebase.database().ref('winner');
 
 function loadPlayerDiv() {
 	$('.load-character').text('');
@@ -40,17 +41,17 @@ $('#player-pick-2').on('click', function() {
 	setTimeout(loadPlayerDiv, 2000);
 })
 
-
+// Set players choice, store in database
 $(document).on('click', '.player-choice', function() {
 	let selection = $(this).attr('id');
 	if (player === '1') {
-		alert(player + selection);
+		// alert(player + selection);
 		p1.set({
 			Choice: selection
 		})
 
 	} else if (player === '2') {
-		alert(player + selection);
+		// alert(player + selection);
 		p2.set({
 			Choice: selection
 		})
@@ -58,8 +59,14 @@ $(document).on('click', '.player-choice', function() {
 	}
 });
 
+
+// at end of game, go to start of page
+$(document).on('click', '#play-again-button', function() {
+	location.reload();
+})
+
 firebase.database().ref().on('value', function(snapshot) {
-	// update ui if players are being used
+	// update player choice ui if players are being used
 	if (snapshot.child('player1').exists()) {
 		$('#player-pick-1').text('');
 		var alreadyPicked = $('<h2>').text('Player 1 is already connected');
@@ -73,37 +80,89 @@ firebase.database().ref().on('value', function(snapshot) {
 		$('#player-pick-2').off('click');
 	}
 
+	// update ui if waiting for other player
+	if (player === '1' && snapshot.child('player1').exists()) {
+		$('.buttons-div').text('');
+		var waitingText = $('<h2>').text('Waiting for other player...');
+		$('.buttons-div').append(waitingText); 
+	};
+
+	if (player === '2' && snapshot.child('player2').exists()) {
+		$('.buttons-div').text('');
+		var waitingText = $('<h2>').text('Waiting for other player...');
+		$('.buttons-div').append(waitingText); 
+	};
 
 
+	// update winner value
 	if (snapshot.child('player1').exists() && snapshot.child('player2').exists()) {
 		if (snapshot.child('player1').val().Choice === 'rock') {
 			if (snapshot.child('player2').val().Choice === 'scissors') {
-				alert('player 1 is the winner');
+				// alert('player 1 is the winner');
+				winner.set({
+					Winner: 'player1'
+				})
 			} else if (snapshot.child('player2').val().Choice === 'rock') {
-				alert('its a tie');
+				// alert('its a tie');
+				winner.set({
+					Winner: 'Tie'
+				})
 			} else {
-				alert('player 2 is the winner');
+				// alert('player 2 is the winner');
+				winner.set({
+					Winner: 'player2'
+				})
 			}
 		} else if (snapshot.child('player1').val().Choice === 'paper') {
 			if (snapshot.child('player2').val().Choice === 'scissors') {
-				alert('player 2 is the winner');
+				// alert('player 2 is the winner');
+				winner.set({
+					Winner: 'player2'
+				})
 			} else if (snapshot.child('player2').val().Choice === 'rock') {
-				alert('player 1 is the winner');
+				// alert('player 1 is the winner');
+				winner.set({
+					Winner: 'player1'
+				})
 			} else {
-				alert('tie');
+				// alert('tie');
+				winner.set({
+					Winner: 'Tie'
+				})
 			}
 
 		} else if (snapshot.child('player1').val().Choice === 'scissors') {
 			if (snapshot.child('player2').val().Choice === 'scissors') {
-				alert('tie');
+				// alert('tie');
+				winner.set({
+					Winner: 'Tie'
+				})
 			} else if (snapshot.child('player2').val().Choice === 'rock') {
-				alert('player 2 is the winner');
+				// alert('player 2 is the winner');
+				winner.set({
+					Winner: 'player2'
+				})
 			} else {
-				alert('player 1 is the winner');
+				// alert('player 1 is the winner');
+				winner.set({
+					Winner: 'player1'
+				})
 			}
 		}
+
+		
+		// remove data for next round
 		p1.remove();
 		p2.remove();
+		// console.log(snapshot.child('winner').val().Winner);
+
+		// update ui on winner
+		$('.buttons-div').text('');
+		var winningText = $('<h2>').text(`${snapshot.child('winner').val().Winner} is the winner`);
+		var playAgainButton = $('<button>').text('Play Again?').attr('id', 'play-again-button');
+		$('.buttons-div').append(winningText, playAgainButton);
+		// remove winner data for next round
+		winner.remove();
 	}
 	// console.log(snapshot);
 	// console.log(snapshot.child('player1').val().Choice);
